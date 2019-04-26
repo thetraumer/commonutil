6617eefc18268b76ac9d4f8ff3214ead7dfa2ec9
vgromov <>
2019-04-26 22:25:39 +0200

    new: added nuget package properties and created the publish script

---
+++ CommonUtil/publishNuget.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = "Stop"
+$version = $args[0]
+$nugetApiKey = $args[1]
+
+dotnet pack -c Release
+cd bin/Release
+dotnet nuget push TheTraumer.CommonUtil.$version.nupkg -k $nugetApiKey -s https://api.nuget.org/v3/index.json
